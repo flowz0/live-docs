92a058fb45114aae591268b5a4ff623ae33b6fe3
flowz0 <billylflowers@gmail.com>
2025-01-22 11:01:58 -0800

    Securing the app by implementing Sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5c8f82c500f12d574d4056e2cc4e1987@o4508688788094976.ingest.us.sentry.io/4508688794058752",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
